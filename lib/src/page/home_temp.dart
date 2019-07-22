d8124c264bca20b7b78723bd39ebefed1a693ef6
Alfredo Arreola <alfredo@sfleet.mx>
2019-07-22 15:35:56 -0500

    cambios

--- lib/src/page/home_temp.dart
+++ lib/src/page/home_temp.dart
@@ -2,10 +2,59 @@ import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
 
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Temp'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(
+        //children: _crearItems(),
+         children: _createItemsCorta(),
+        /* <Widget>[
+          ListTile(
+            title: Text('Hola mundo')
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Hola mundo')
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Hola mundo')
+          ),
+        ], */
+      ),
     );
+  }
+  /* List<Widget> _crearItems(){
+    List<Widget> lista = new List<Widget>();
+    for (var opt in opciones){
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+      lista.add(tempWidget);
+      lista.add(Divider(color: Colors.blue,));
+    }
+    return lista;
+  } */
+  
+  List<Widget> _createItemsCorta(){
+    return opciones.map((item){
+      return Column(
+        children: <Widget>[
+          ListTile(
+            title: Text(item + '!'),
+            subtitle: Text('Cualquier cosa'),
+            leading:Icon(Icons.android, size: 30.0,),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap:(){},
+          ),
+          Divider()
+        ],
+      );
+    }).toList();
   }
 }
