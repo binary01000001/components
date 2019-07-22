a8d7dfe387ce39f6f526623e2a0bb5be13ece35d
Alfredo Arreola <alfredo@sfleet.mx>
2019-07-22 18:20:39 -0500

    cambios

--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:componentes/src/page/alert_page.dart';
 import 'package:componentes/src/providers/menu_provider.dart';
+import 'package:componentes/src/utils/icono_string_util.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -30,7 +32,7 @@ class HomePage extends StatelessWidget {
         //print('builder');
         print( snapshot.data);
         return ListView(
-          children: _listaItems(snapshot.data),
+          children: _listaItems(snapshot.data, context),
         );
       },
     );
@@ -45,15 +47,19 @@ class HomePage extends StatelessWidget {
       ListTile(title: Text('Hola mundo')),
     ];
   } */
-  List<Widget> _listaItems(List<dynamic> data) {
+  List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
    final List<Widget> opciones = [];
    data.forEach((opt){
      final widgetTemp = ListTile(
        title: Text(opt['texto']),
-       leading: Icon(Icons.account_circle, color: Colors.blue,),
+       //leading: Icon(Icons.account_circle, color: Colors.blue,),
+       leading: getIcon(opt['icon']),
        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue) ,
        onTap:(){
-
+         final route = MaterialPageRoute(
+           builder:(context) => AlertPage()
+        );
+         Navigator.push(context, route);
        }
        );
        opciones ..add(widgetTemp)
